c470ab9d49993d111697cc29faca5519a495d253
ozod07 <xolmurodovozodbek96@gmail.com>
2024-06-19 08:49:29 +0500

    about page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/common/Navbar"
 import HomePage from "./pages/HomePage"
 import ContactPage from "./pages/ContactPage"
+import AboutPage from "./pages/AboutPage"
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
     </div>
   )
